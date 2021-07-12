487452663d61ef7f5223de74413baaacafcd1520
Shashank Aital <aitalshashank2@gmail.com>
2021-07-13 00:29:14 +0530

    [Frontend] Add documentation for dashboard

--- frontend/src/dashboard/DashboardContainer.js
+++ frontend/src/dashboard/DashboardContainer.js
@@ -6,24 +6,33 @@ import DashboardComponent from "./components/DashboardComponent"
 import Nav from "../common/Nav/NavContainer"
 import Notification from "../common/Notification/NotificationController"
 
+/**
+ * Container for the Dashboard Component
+ * 
+ * This component handles the logic for calling various components based on user login state
+ * 
+ * @returns {JSX.Element} Dashboard
+ */
 const Dashboard = () => {
 
     const apiState = useSelector(state => state.user.apiState)
     const userDetails = useSelector(state => state.user.userDetails)
     const dispatch = useDispatch()
 
+    // Handle when the user clicks a meeting tile
     const handleTeamClick = (teamCode) => {
         window.location = `/teams/${teamCode}`
     }
 
+    // Handle when the user clicks the `new meeting` option
     const handleCreateJoinTeam = () => {
         window.location = "/teams/new"
     }
 
-    if(apiState === "norequest"){
+    if (apiState === "norequest") {
         performVerify(dispatch)
         return <Redirect to="/loader" />
-    }else{
+    } else {
 
         return (
             <>
